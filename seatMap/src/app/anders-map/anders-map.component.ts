df93f34b4d6080e1017bfe20138c24bc3e2d5ffa
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2018-10-01 15:37:53 -0600

    3d box scroll renders but doesnt scroll

--- seatMap/src/app/anders-map/anders-map.component.ts
+++ seatMap/src/app/anders-map/anders-map.component.ts
@@ -1,73 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as THREE from 'three';
-let camera, scene, renderer;
-
 @Component({
   selector: 'app-anders-map',
   templateUrl: './anders-map.component.html',
   styleUrls: ['./anders-map.component.scss']
 })
 export class AndersMapComponent implements OnInit {
-  constructor() { }
+  scene: any = null;
+  camera: any = null;
+  renderer: any = null;
+  geometry: any = null;
+  material: any = null;
 
+  constructor() {
+    this.geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    this.material = new THREE.MeshNormalMaterial();
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
   ngOnInit() {
-    this.init();
+    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    this.camera.position.set(0, 5, 2);
+    console.log(this.camera.position, 'cool!');
+    this.scene = new THREE.Scene();
+    document.body.appendChild(this.renderer.domElement);
+    window.addEventListener('touchstart', this.onMouseWheel, {passive: false});
+    window.addEventListener('resize', this.onWindowResize, {passive: false});
+
+    // generate some boxes in a column
+
+    for (let i = 0; i <= 10; i++) {
+      const mesh = new THREE.Mesh(this.geometry, this.material);
+      mesh.position.y = i;
+      this.scene.add(mesh);
+    }
+
     this.animate();
+    }
+
+
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
+    this.renderer.render(this.scene, this.camera);
   }
 
-  init() {
+  onMouseWheel(event) {
+    console.log(event);
+    event.preventDefault();
+    // console.log(this.scene);
+    // console.log(this.renderer);
+    console.log(this.camera);
+    this.camera.position.y -= event.deltaY * 0.005;
 
-  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-  camera.position.set(0, 5, 2);
+    // prevent scrolling beyond a min/max value
 
-  scene = new THREE.Scene();
-
-  const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-  const material = new THREE.MeshNormalMaterial();
-
-  // generate some boxes in a column
-
-  for (let i = 0; i <= 10; i++) {
-
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.position.y = i;
-    scene.add(mesh);
+    this.camera.position.clampScalar(0, 10);
 
   }
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+  onWindowResize() {
 
-    window.addEventListener('wheel', this.onMouseWheel, false);
-    window.addEventListener('resize', this.onWindowResize, false);
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
 
   }
-
- animate() {
-
-  requestAnimationFrame(this.animate);
-  renderer.render(scene, camera);
-}
-
-onMouseWheel(event) {
-
-  event.preventDefault();
-
-  camera.position.y -= event.deltaY * 0.005;
-
-  // prevent scrolling beyond a min/max value
-
-  camera.position.clampScalar(0, 10);
-
-}
-
-onWindowResize() {
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-}
-
 }
